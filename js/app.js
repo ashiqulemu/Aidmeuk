1dd27c74c1f75eaf35d319a088de62cb50c6e222
ashiqulemu <ashiqulemu.jpi@gmail.com>
2023-07-12 12:11:16 +0600

    sadasd

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 4000,
   wrap: true
 })
+
+
+function catchAmount(event){
+  console.log(event.target.value)
+  document.getElementById("donate").innerHTML = "$"+ event.target.value
+}
+
+function onchange(event){
+  console.log(event);
+}
